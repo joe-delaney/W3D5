c9b36c294cfd874e94e86204a6b0c429e379e98b
App Academy Student <email@email.com>
2022-02-18 12:13:51 -0500

    phase 1

--- knights_travails/knight_path_finder.rb
+++ knights_travails/knight_path_finder.rb
@@ -1,11 +1,32 @@
 class KnightPathFinder
+  def self.valid_moves(pos)
+
+  end
+
+  def initialize(start_pos)
+    @root_node = PolyTreeNode.new(start_pos)
+    @considered_positions = [start_pos]
+    build_move_tree
+  end
 
   def build_move_tree
 
   end
 
-  def initialize(start_position)
-    @root_node = PolyTreeNode.new(start_position)
-    build_move_tree
+  def new_move_positions(pos)
+    #Find all possible moves from valid moves
+    possible_moves = KnightPathFinder.valid_moves(pos)
+
+    #Only include possible moves not yet considered
+    available_new_moves = possible_moves.select do |move| 
+      !considered_positions.include?(move)
+    end
+
+    #Add available new moves to considered positions and return available new moves
+    considered_positions.concat(available_new_moves)
+    available_new_moves
   end
+
+  private
+  attr_reader :considered_positions
 end
